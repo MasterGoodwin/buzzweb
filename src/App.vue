fb4dc7e5a41109f62b6b8fa7c9cbb44d90666fda
Denis Maenko <yes@vipdv.ru>
2020-05-25 23:55:35 +1000

    Yarrrr

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,130 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app>
+        <div v-for="block in blocks" :key="block.index" :class="block.type" @click.stop="openSettings(block.index)" :style="blockStyle(block)">
+            <div v-if="block.type === 'fhh'" class="wrap">
+                <h1 class="display-3 white--text my-5">{{ block.options.header }}</h1>
+                <p class="headline white--text">{{ block.options.description }}</p>
+            </div>
+            <div v-if="block.type === 'col3'" class="wrap">
+                <v-container>
+                    <v-row>
+                        <v-col cols="12" sm="4">
+                            <h2><v-icon class="header-icon">mdi-{{ block.options.icon1 }}</v-icon>{{ block.options.header1 }}</h2>
+                            <p>{{ block.options.description1 }}</p>
+                        </v-col>
+                        <v-col cols="12" sm="4">
+                            <h2><v-icon class="header-icon">mdi-{{ block.options.icon2 }}</v-icon>{{ block.options.header2 }}</h2>
+                            <p>{{ block.options.description2 }}</p>
+                        </v-col>
+                        <v-col cols="12" sm="4">
+                            <h2><v-icon class="header-icon">mdi-{{ block.options.icon3 }}</v-icon>{{ block.options.header3 }}</h2>
+                            <p>{{ block.options.description3 }}</p>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </div>
+        </div>
+
+        <Adder v-on:add-block="addBlockHandler" :fh="this.blocks.length === 0"></Adder>
+
+        <v-navigation-drawer v-model="settingsList" fixed temporary>
+            <v-container v-for="(value, name) in activeSettings" :key="name">
+                <v-text-field :label="name" :placeholder="name" outlined v-model="activeSettings[name]" v-on:change="updateOption(name, value)"></v-text-field>
+            </v-container>
+        </v-navigation-drawer>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Adder from './components/AdderBox';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+
+        components: {
+            Adder,
+        },
+
+        data () {
+            return {
+                settingsList: false,
+                activeSettings: {},
+                activeBlock: null,
+                blocks: []
+            }
+        },
+        methods: {
+            addBlockHandler: function(block) {
+                let newBlock = JSON.parse(JSON.stringify(block));
+                newBlock.index = this.blocks.length;
+                this.blocks.push(newBlock);
+            },
+            openSettings(index) {
+                this.activeSettings = this.blocks[index].options;
+                this.activeBlock = index;
+                this.settingsList = true;
+            },
+            updateOption(name, value) {
+                // console.log(this.blocks[this.activeBlock], name, value);
+                this.blocks[this.activeBlock].options[name] = value;
+            },
+            blockStyle(block) {
+                let style = {};
+                if (block.options.bgImage !== undefined) {
+                    style.backgroundImage = "url(../img/" + block.options.bgImage + ")";
+                }
+                // console.log(style);
+                return style;
+            }
+        },
+        mounted() {
+            // this.$on('addBlock', this.addBlockHandler)
+        }
+    };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="scss">
+    .fhh {
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        overflow: hidden;
+        position: relative;
+        text-align: center;
+
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-attachment: fixed;
+        background-color: #000;
+        /*background-image: url(./assets/img1.jpg);*/
+
+        &:after {
+            content: '';
+            display: block;
+            width: 100%;
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 100vh;
+            background-image: linear-gradient(to top, rgba(26,40,120,0.70), rgba(10,143,196,0.70));
+        }
+
+        .wrap {
+            position: relative;
+            z-index: 1;
+
+        }
+    }
+    .col3 {
+        .header-icon {
+            margin: 20px;
+            color: blue;
+        }
+        p {
+            padding: 10px 20px;
+        }
+    }
 </style>
---
+++ src/components/AdderBox.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="py-0" :class="{'fill-height': fh}">
+        <v-container fluid class="fill-height py-0">
+            <v-row class="adder-box">
+                <v-col cols="12" class="py-0">
+                    <v-row align="center" justify="center" class="adder-box-inner">
+                        <v-btn color="pink" fab dark @click.stop="adderList = !adderList">
+                            <v-icon dark>mdi-shape-square-plus</v-icon>
+                        </v-btn>
+                    </v-row>
+                </v-col>
+            </v-row>
+        </v-container>
+        <v-navigation-drawer v-model="adderList" fixed temporary>
+            <v-list>
+                <v-list-item v-for="item in availableBlocks" :key="item.title" link @click="addBlock(item)">
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AdderBox",
+        props: ['fh'],
+        data () {
+            return {
+                adderList: false,
+                availableBlocks: [
+                    {
+                        title: 'Full-height Header',
+                        icon: 'mdi-image-area',
+                        type: 'fhh',
+                        options: {
+                            bgImage: 'img1.jpg',
+                            header: 'Big Header Example',
+                            description: 'First block is the biggest one'
+                        }
+                    },
+                    {
+                        title: 'Three column articles',
+                        icon: 'mdi-view-column',
+                        type: 'col3',
+                        options: {
+                            icon1: 'alert-decagram',
+                            header1: 'First column',
+                            description1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mollis nunc sed id semper risus in hendrerit gravida rutrum. Ante in nibh mauris cursus mattis molestie a iaculis at. Id semper risus in hendrerit. Tincidunt augue interdum velit euismod in pellentesque massa. Nam aliquam sem et tortor consequat.',
+                            icon2: 'alarm',
+                            header2: 'First column',
+                            description2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mollis nunc sed id semper risus in hendrerit gravida rutrum. Ante in nibh mauris cursus mattis molestie a iaculis at. Id semper risus in hendrerit. Tincidunt augue interdum velit euismod in pellentesque massa. Nam aliquam sem et tortor consequat.',
+                            icon3: 'asterisk',
+                            header3: 'First column',
+                            description3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mollis nunc sed id semper risus in hendrerit gravida rutrum. Ante in nibh mauris cursus mattis molestie a iaculis at. Id semper risus in hendrerit. Tincidunt augue interdum velit euismod in pellentesque massa. Nam aliquam sem et tortor consequat.',
+                        }
+                    },
+                ],
+            }
+        },
+        methods: {
+            addBlock(block) {
+                this.$emit('add-block', block);
+                this.adderList = false;
+            }
+        }
+
+    }
+</script>
+
+<style scoped lang="scss">
+    .adder-box {
+        height: 100%;
+        min-height: 200px;
+        padding: 20px;
+        .adder-box-inner {
+            height: 100%;
+            background-color: #eeeeee;
+            border: 4px dashed #dddddd;
+            &:hover {
+                background-color: #e5e5e5;
+            }
+        }
+    }
+</style>
